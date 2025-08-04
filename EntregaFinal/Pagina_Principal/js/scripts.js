ce7e203967c77717b1398a40a63c55e1e5a79f39
Edenogue <88842426+Edenogue@users.noreply.github.com>
2025-08-04 13:41:04 -0300

    Agregar login temporal con menú de perfil

--- EntregaFinal/Pagina_Principal/js/scripts.js
+++ EntregaFinal/Pagina_Principal/js/scripts.js
@@ -1,1 +1,25 @@
-// Empty JS for your own code to be here
+$(document).ready(function () {
+  function updateNav() {
+    const loggedIn = localStorage.getItem('loggedIn') === 'true';
+    if (loggedIn) {
+      $('#loginNav').hide();
+      $('#perfilDropdown').show();
+    } else {
+      $('#loginNav').show();
+      $('#perfilDropdown').hide();
+    }
+  }
+
+  $('#loginBtn').on('click', function () {
+    localStorage.setItem('loggedIn', 'true');
+    $('#loginModal').modal('hide');
+    updateNav();
+  });
+
+  $('#logoutBtn').on('click', function () {
+    localStorage.removeItem('loggedIn');
+    updateNav();
+  });
+
+  updateNav();
+});
