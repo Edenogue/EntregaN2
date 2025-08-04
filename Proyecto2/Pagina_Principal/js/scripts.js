1a949d10acd94deba338551d787fc64da0707e04
Edenogue <88842426+Edenogue@users.noreply.github.com>
2025-08-04 14:14:36 -0300

    Reduce header and unify footer color

--- Proyecto2/Pagina_Principal/js/scripts.js
+++ Proyecto2/Pagina_Principal/js/scripts.js
@@ -1,1 +1,10 @@
-// Empty JS for your own code to be here
+$(function() {
+    $('.navbar-brand img').hover(
+        function() {
+            $(this).css('box-shadow', '0 0 10px 2px rgba(255, 215, 0, 0.6)');
+        },
+        function() {
+            $(this).css('box-shadow', 'none');
+        }
+    );
+});
